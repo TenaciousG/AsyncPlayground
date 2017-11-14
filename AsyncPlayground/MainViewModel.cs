02e53dea4ec2d3578d6f5038d53246e355e9ea03
Glen Olsen <gol@dips.no>
2017-11-14 14:26:16 +0100

    first cm

---
+++ AsyncPlayground/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AsyncPlayground.Annotations;
+using AsyncPlayground.Commands;
+
+namespace AsyncPlayground
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string m_fetchResult;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand FetchSomethingCommand { get; }
+
+        public string FetchResult
+        {
+            get { return m_fetchResult; }
+            set
+            {
+                m_fetchResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            FetchSomethingCommand = new AwaitableDelegateCommand(FetchSomething);
+            FetchResult = "Initial";
+        }
+
+        private async Task FetchSomething()
+        {
+            var timeConsumingTask = DoSomethingThatTakesAWhile();
+
+            int a = DoSomethingQuickMeanwhile();
+
+            var fetchResult = await timeConsumingTask;
+            FetchResult = $"fetched {fetchResult}";
+
+        }
+
+        private static int DoSomethingQuickMeanwhile()
+        {
+            return 1 + 2;
+        }
+
+        private async Task<DateTime> DoSomethingThatTakesAWhile()
+        {
+            await Task.Delay(5000);
+            return DateTime.Now;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
